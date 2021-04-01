52fa7a189532c39de386f4bd068e8042dbed9cf1
kid1412 <kid1412@gdjd.cn>
2021-04-01 09:26:39 +0800

    登录

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const server=express()
 const cors=require('cors')
 server.use(cors())
 
+ const jwt = require('express-jwt');
+server.use(jwt({
+    secret: 'gz61', // 生成token时的钥匙
+    algorithms: ['HS256'] // 加密算法
+  }).unless({
+    path: ['/api/login', '/api/register', /^\/uploads\/.*/]
+  }));
 /* 测试
 server.use((req,res)=>{
     res.end('ok')
@@ -16,6 +23,14 @@ const myRouter=require('./router/my_router.js')
 const apiRouter=require('./router/api.js')
 server.use('/my',myRouter)
 server.use('/api',apiRouter)
+
+// 错误处理中间件
+server.use((err, req, res, next) => {
+    console.log('有错误', err)
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).send({ code: 1, message: '身份认证失败！' });
+    }
+  });
 
 server.listen(3000,()=>{
     console.log('接口完成');
--- router/api.js
+++ router/api.js
@@ -1,12 +1,66 @@
-const express=require('express')
-const router=express.Router()
-// const conn=require('../util/sql.js')
+const express = require('express')
+const router = express.Router()
+const conn = require('../util/sql.js')
+const jwt = require('jsonwebtoken')
+router.use(express.urlencoded())
 // 注册接口
-router.post('/reguser',(req,res)=>{
-    res.end('ok')
+router.post('/reguser', (req, res) => {
+    // res.end('ok')
+    console.log('获取参数', req.body);
+    const { username, password } = req.body;
+    const sqlStr = `select username from users where username="${username}"`
+    conn.query(sqlStr, (err, result) => {
+        if (err) {
+            console.log(err)
+            res.json({ code: 500, msg: "服务器错误" })
+            return
+        }
+        console.log(result)
+        // 用户名被占用了
+        if (result.length > 0) {
+            res.json({ code: 201, msg: "注册失败，名字占用了" })
+            return
+        }
+        // 未被使用的用户名
+        const sqlStr = `insert into users (username, password) values ("${username}", "${password}")`
+        conn.query(sqlStr, (err, result) => {
+            console.log(err)
+            console.log(result)
+            if (err) {
+                res.json({ code: 500, msg: "服务器错误" })
+                return
+            }
+            res.json({ "status": 0, code: 200, msg: '注册成功' })
+        })
+    })
 })
 // 登录接口
-router.post('/login',(req,res)=>{
-    res.end('ok')
+router.post('/login', (req, res) => {
+    // res.end('ok')
+    console.log('获取参数', req.body);
+    const { username, password } = req.body;
+    const sqlStr = `select * from users where username="${username} and password="${password}"`
+    conn.query(sqlStr, (err, result) => {
+        if (err) {
+            res.json({ code: 500, msg: "服务器错误" })
+            return
+        }
+        console.log(result);
+        if (result.length > 0) {
+            const token = 'Bearer' + jwt.sign(
+                { name: username },
+                'gz61',
+                { expiresIn: 2 * 60 * 60 }
+            )
+            res.json({
+                "status": 0,
+                "code": 200,
+                "message": "登录成功！",
+                "token": token
+            })
+        } else {
+            res.json({ code: 201, msg: "登陆失败，用户名密码不对" })
+        }
+    })
 })
 module.exports = router
